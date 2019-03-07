d45631f5e2449e9e6e6203dbd3669cf7a69676cc
Jakub Bujnowicz <bujnowiczjakub@gmail.com>
2019-03-07 21:33:24 +0100

    Zmiana dla liczb wykraczających poza zakres

--- cw1/zestaw23.R
+++ cw1/zestaw23.R
@@ -7,11 +7,18 @@ na_rzymskie <- function(numbers) {
     # Udało mi się znależć niektóre źródła, które mówią o symbolach dla 5000 wzwyż,
     # zwykle z linią nad, ale nie mam pojęcia na implementację. Stąd skrócony zakres
     # obsługi liczb wejściowych.
-    if (!is.numeric(numbers) || !numbers %in% 1:3999) {
-        stop("Liczby muszą być naturalne i nie większe niż 3999.")
+    if (!is.numeric(numbers)) {
+        stop("Liczby muszą być naturalne.")
+    }
+    if (any(!numbers %in% 1:3999)) {
+        warning("Niektóre liczby nie są naturalnymi z zakresu 1-3999 - zwrócono NA.")
     }
 
     single <- function(number) {
+        if (!number %in% 1:3999) {
+            return(NA)
+        }
+
         breakpoints <- c(1000, 500, 100, 50, 10, 5, 1)
         signs <- c("M", "D", "C", "L", "X", "V", "I")
         counts <- numeric(length(breakpoints))
@@ -49,3 +56,6 @@ na_rzymskie <- function(numbers) {
 # numbers <- c(1, 3, 4, 5, 8, 9, 10)
 # numbers <- c(numbers, numbers * 10, numbers * 100, 3000, 3700, 3999)
 # setNames(na_rzymskie(numbers), numbers)
+#
+# na_rzymskie(c(0, 444, 4000, 1.5))
+
