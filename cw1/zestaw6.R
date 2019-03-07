c76c430105f0a9165367e4756c9403bc41ebdcda
anitaksiezak <45211692+anitaksiezak@users.noreply.github.com>
2019-03-07 22:47:51 +0100

    Wersja po uwzględnieniu uwag.

--- cw1/zestaw6.R
+++ cw1/zestaw6.R
@@ -5,17 +5,16 @@
 
 mniejsza_od_pierwiastka = function(liczba){
   
-  if(typeof(liczba)!="double"){
+  if(!is.numeric(liczba)){
     return(cat('Argument nie jest liczbą!'))
   } else if (liczba<0) {
-    wynik <- sqrt(as.complex(liczba))
-    return(cat('Pierwiastek z liczby ujemnej',liczba,'jest liczbą zespoloną równą',wynik,'.\nNajwiększą liczbę całkowitą mniejszą od pierwiastka z liczby ujemnej obliczamy dzięki funkji podłoga,\njednak funkcja ta operuje jedynie na zbiorze liczb rzeczywistych.'))
+    return(cat('Pierwiastek z liczby ujemnej',liczba,'jest liczbą zespoloną.\nNajwiększą liczbę całkowitą mniejszą od pierwiastka z liczby ujemnej obliczamy dzięki funkji podłoga,\njednak funkcja ta operuje jedynie na zbiorze liczb rzeczywistych.'))
   } else {
-    wynik <- floor(sqrt(liczba))
-    #Dla liczby 16 pierwiastek wynosi 4. Zatem liczba całkowita mniejsza od pierwiastka z 16 (4) wynosi 3.
-    if(wynik*wynik==liczba){
-      wynik <- wynik - 1
+    wynik <- 0
+    while(wynik * wynik < liczba){
+      wynik <- wynik + 1
     }
+      wynik <- wynik - 1
     return(cat('Największą liczbą całkowitą mniejszą od pierwiastka z',liczba,'jest',wynik,'.'))
   }
 }
