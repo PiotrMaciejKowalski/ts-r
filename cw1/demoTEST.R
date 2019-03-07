16ca75ec065e3c057ab89af3cebb5f925060f1a4
PiotrMaciejKowalski <piotr.maciej.kowalski@gmail.com>
2019-03-07 11:40:45 +0100

    Update demoTEST.R

--- cw1/demoTEST.R
+++ cw1/demoTEST.R
@@ -13,3 +13,18 @@ if (result == exp_result){
 } else {
   print('PORAŻKA')
 }
+
+## TEST CASE 2
+## miejsce zerowe dla y=0x+0
+## funkcja ta ma nieskonczenie wiele miejsc zerowych
+## przyjmuje zatem ze wtedy zwraca 0 jako przyklad takiego miejsca
+
+a =0
+b = 0
+exp_result = 0
+result = miejsce_zerowe(a, b)
+if (result == exp_result){
+  print('SUKCES')
+} else {
+  print('PORAŻKA')
+} 
