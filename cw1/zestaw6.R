af89264354145578793b578e2d58dd073d766453
anitaksiezak <45211692+anitaksiezak@users.noreply.github.com>
2019-03-09 13:47:43 +0100

    Rozwiązanie zestawu 6 v3

--- cw1/zestaw6.R
+++ cw1/zestaw6.R
@@ -8,13 +8,16 @@ mniejsza_od_pierwiastka = function(liczba){
   if(!is.numeric(liczba)){
     return(cat('Argument nie jest liczbą!'))
   } else if (liczba<0) {
-    return(cat('Pierwiastek z liczby ujemnej',liczba,'jest liczbą zespoloną.\nNajwiększą liczbę całkowitą mniejszą od pierwiastka z liczby ujemnej obliczamy dzięki funkji podłoga,\njednak funkcja ta operuje jedynie na zbiorze liczb rzeczywistych.'))
+    return(NA)
+    #Pierwiastek z liczby ujemnej jest liczbą zespoloną.
+    #Największą liczbę całkowitą mniejszą od pierwiastka z liczby ujemnej obliczamy dzięki funkji podłoga,
+    #jednak funkcja ta operuje jedynie na zbiorze liczb rzeczywistych.
   } else {
     wynik <- 0
     while(wynik * wynik < liczba){
       wynik <- wynik + 1
     }
       wynik <- wynik - 1
-    return(cat('Największą liczbą całkowitą mniejszą od pierwiastka z',liczba,'jest',wynik,'.'))
+    return(wynik)
   }
 }
