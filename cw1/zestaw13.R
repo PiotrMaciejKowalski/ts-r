46834551a0808b3f4b0308ace32cf2945bfb26b7
Aleksandra Kowarik <olakowarik98@gmail.com>
2019-03-10 22:19:21 +0100

    poprawione

--- cw1/zestaw13.R
+++ cw1/zestaw13.R
@@ -4,7 +4,7 @@
 
 ilosc_spolglosek = function(slowo){
   slowo1 <- tolower(slowo)
-  slowo_bez_samoglosek <- gsub("[ aeiouy]","", slowo1)
-  ile <- nchar(slowo_bez_samoglosek)
+  slowo_bez_spolglosek <- gsub("[qwrtplkjhgfdszxcvbnmQWRTPLKJHGFDSZXCVBNM]","", slowo1)
+  ile <- nchar(slowo)-nchar(slowo_bez_spolglosek)
   return(ile)
 }
