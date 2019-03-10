bb7c3ea6bde45efbd1653e023f777c8606ab49d5
Aleksandra Kowarik <olakowarik98@gmail.com>
2019-03-10 19:32:20 +0100

    zestaw13 - rozwiazanie

--- cw1/zestaw13.R
+++ cw1/zestaw13.R
@@ -2,6 +2,9 @@
 
 ## Przygotowac funkcje, ktora podaje liczbe spółgłosek w zadanym słowie
 
-ilosc_spolglosek = function(){
-  
+ilosc_spolglosek = function(slowo){
+  slowo1 <- tolower(slowo)
+  slowo_bez_samoglosek <- gsub("[aeiouy]","", slowo1)
+  ile <- nchar(slowo_bez_samoglosek)
+  print(ile)
 }
