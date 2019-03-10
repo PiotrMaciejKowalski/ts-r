1d999f6ad8d1c42b558855db7ab6c6f80b21e73e
Joannna Szymanska <joannaszymanska131@gmail.com>
2019-03-10 22:54:01 +0100

    zestaw 4 v3

--- cw1/zestaw4.R
+++ cw1/zestaw4.R
@@ -14,23 +14,19 @@ moda_z_tekstu = function(zdanie){
     n=gsub(x[j],"",zdanie_malymi_literami)
     ilosc_liter_w_zdaniu[j]=nchar(zdanie_malymi_literami)-nchar(n)
   }
-  # while(j<27){
-  #   n=gsub(x[j],"",zdanie_malymi_literami)
-  #   ilosc_liter_w_zdaniu[j]=nchar(zdanie_malymi_literami)-nchar(n)
-  #   j<-j+1
-  # }
-  while(i<27){
+
+  for (i in 1:26){
     if( ilosc_liter_w_zdaniu[i]>max){
-       max= ilosc_liter_w_zdaniu[i]
-       } 
-    i=i+1}
+       max= ilosc_liter_w_zdaniu[i]}
+        
+    }
   i2=1
   licz=0
-  while(i2<27){
+  for (i2 in 1:26){
     if(max>0 &&  ilosc_liter_w_zdaniu[i2]==max){
       licz=licz+1
       moda[licz]=x[i2]}
-    i2=i2+1
+    
   }
 
   return(moda)
