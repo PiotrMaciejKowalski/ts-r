59bc297a88aa3f02f57433c8830bd55c14ff62b8
Natalia Kielbik <natalia.kielbik306@gmail.com>
2019-03-12 23:16:32 +0100

    Rozwiązanie zestawu 15

--- cw1/zestaw15.R
+++ cw1/zestaw15.R
@@ -4,7 +4,14 @@
 ## pelny -
 
 podrozujacy_x = function(dlugosc_linii){
+  line <- rep("-", dlugosc_linii)
   
+  for(position in 1:dlugosc_linii){
+    currentLine <- line
+    currentLine[position] <- "X"
+    readyLine <- paste(currentLine, collapse = '')
+    print(readyLine)
+  }
 }
 
 ## np
