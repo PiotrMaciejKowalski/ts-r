16a496eedf09932eb06f8575f9a959e63c9a6a63
Weronika Lara <weronika.lara23@gmail.com>
2019-03-12 09:36:24 +0100

    Dodano rozwiązanie zestawu 12

--- cw1/zestaw12.R
+++ cw1/zestaw12.R
@@ -3,9 +3,14 @@
 ## Przygotowac funkcje, ktora sprawdza czy w danym slowie znajduje sie litera a potem zwraca 
 ## wektor pozycji na ktorych zostaly odszukane te litery. Niech domyslnie ignoruje wielkosc liter
 
-znajdz_litere = function(slowo, litera){
-  
+znajdz_a1=function(wektor){
+  grep("a",wektor)
 }
+##przyklad
+##znajdz_a1(c("l","a","r","a"))
+
+  
+
 
 ## przyklad
 ## znajdz_litere('Matematyka', 'a') -> c(2,6,10) 
