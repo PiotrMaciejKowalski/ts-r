a345202b654e93a0b1d04b67b4705e044e42eeae
Karolina Mruk <kmruk97@gmail.com>
2019-03-13 13:47:33 +0100

    zestaw9 proba

--- cw1/zestaw9.R
+++ cw1/zestaw9.R
@@ -4,5 +4,15 @@
 ## przekazanym przez parametr
 
 zamiana_liter = function(slowo){
-  
+  wektor <- strsplit(slowo,"")
+  wektor2 <- unlist(wektor)
+  ilosc_elementow <- length(wektor2)
+  wektor3 <- wektor2
+  for (i in 1:ilosc_elementow){
+    if (wektor2[i]=="[zxcvbnmasdfghjklqwertyuiop]"){
+      wektor3[i] =toupper(wektor2[i])}
+    else if (wektor2[i]=="[ZXCVBNMASDFGHJKLQWERTYUIOP]"){
+      wektor3[i]=tolower(wektor2[i])}
+    }
+  return(wektor3)
 }
