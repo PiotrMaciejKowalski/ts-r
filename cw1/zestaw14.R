f33da1560b62e79ef8f0b271364ac495bc697d65
Katarzyna Łądka <kejti0307@gmail.com>
2019-03-17 23:46:02 +0100

    rozwiazanie   cw1/zestaw14

--- cw1/zestaw14.R
+++ cw1/zestaw14.R
@@ -3,14 +3,14 @@
 ## Napisać funkcję, ktora dla danego wektora liczb wylicza sume drugiej najmniejszej liczby i 
 ## drugiej największej. Działa przy założeniu, że wektor ma conajmniej 3 elementy
 
-suma_vice_ekstremum = function(wektor){
-  wektor1 <-unlist(strsplit(wektor,""))
-  wektor_posortowany_rosnaco <-sort(wektor1)
-  dlugosc_wektora <- length(wektor_posortowany_rosnaco)
-  vice_maks <- as.numeric(wektor_posortowany_rosnaco[dlugosc_wektora-1])
-  vice_min <- as.numeric(wektor_posortowany_rosnaco[2])
+suma_vice_ekstremum = function(wektor1){
+  wektor11 <-unlist(strsplit(wektor1,""))
+  wektor1_posortowany_rosnaco <-sort(wektor11)
+  dlugosc_wektora1 <- length(wektor1_posortowany_rosnaco)
+  vice_maks <- as.numeric(wektor1_posortowany_rosnaco[dlugosc_wektora1-1])
+  vice_min <- as.numeric(wektor1_posortowany_rosnaco[2])
   suma <- vice_maks+vice_min
-  if (dlugosc_wektora<3){
+  if (dlugosc_wektora1<3){
     suma <- "podaj minimum 3 elementy wektora"
   }
   return(suma)
