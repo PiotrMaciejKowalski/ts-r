89ef27a6afb811e5ff85ee0b2b47d7bbe12f52aa
PiotrMaciejKowalski <piotr.maciej.kowalski@wp.pl>
2019-03-18 18:06:39 +0100

    Dodanie projektu 3

---
+++ cw3/zadanie.R
@@ -0,0 +1,12 @@
+## Zadanie
+
+## W zadaniu nalezy powrócić do danych zaczytanych w poprzednim zadaniu.
+
+## Należy 
+## * przekopionować poprzedni plik markdown (wersję z cw2 zostawiamy jako zakończoną)
+## * poniżej należy rozważyć dwa rodzaje modeli (proszę wybrać pojedyncza zmienna, tzw. np cene otwarcia)
+## ** model z trendem liniowych dla calych przeszlych danych
+## ** model z trendem liniowych korzystajacy zawsze ze stalej liczby bezposrednio poprzedzajacych probek
+## * wygenerowac wykres prognozy na dane momenty
+## * wyznaczyc błąd średniokwadratowy na jedna chwilę czasu wprzód
+## * wyznaczyć wartość statystykie Theil'a dla tak opracowanej prognozy.
