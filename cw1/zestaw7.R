2d0701e33e33b28a63cba86761b46094db01c327
Patrycja Kolanwska <patrycja.kolanowska@onet.eu>
2019-03-18 10:32:29 +0100

    Rozwiązanie zestawu 7

--- cw1/zestaw7.R
+++ cw1/zestaw7.R
@@ -1,7 +1,9 @@
 ## Zadanie
 
 ## Przygotować funkcję, która dla danej liczby znajduje sumę jej cyfr
-
-suma_cyfr = function(liczba){
-  
+suma_cyfr <- function(liczba){
+  digitsum <- sum(floor(liczba / 10^(0:(nchar(liczba) - 1))) %% 10)
 }
+
+suma = suma_cyfr(liczba = 251)
+print(suma)
