6c18aba1c906823305140ff7dcdc6368463fbf26
Adam Malarski <adammalarski1210@gmail.com>
2019-03-19 17:24:34 +0100

    dodano rozwiazanie zadania 10

--- cw1/zestaw10.R
+++ cw1/zestaw10.R
@@ -3,6 +3,30 @@
 ## Napisać funkcję, która sprawdzi czy w podanym wyrażeniu algebraicznym są poprawnie zadane nawiasy
 ## Np 3*(2+4*4)-2+(3+4^2)
 
-sprawdzacz_nawiasow = function(){
-  
+sprawdzacz_nawiasow = function(wyrazenie){
+  wyrazenie1 <-unlist(strsplit(wyrazenie,""))
+  licznik <- 0
+  dlugosc_wyrazenia <- length(wyrazenie1)
+  nawiasy <- "nawiasy poprawne"
+  for(i in 1:dlugosc_wyrazenia){
+    if (wyrazenie1[i]=="("){
+      licznik = licznik+1
+    }
+    if (wyrazenie1[i]==")"){
+      licznik = licznik-1
+    }
+  }
+  if (licznik!=0){
+    nawiasy = "�le zadane nawiasy"
+  }
+  otwarcie_nawiasu <- grep("[(]", wyrazenie1)
+  zamkniecie_nawiasu <- grep("[)]", wyrazenie1)
+  n <- length(otwarcie_nawiasu)
+  roznica <- otwarcie_nawiasu-zamkniecie_nawiasu
+  for (j in 1:n) {
+    if (roznica[j]>0){
+      nawiasy = "�le zadane nawiasy"
+    }
+  }
+  return(nawiasy)
 }
