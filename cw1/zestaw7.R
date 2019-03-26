27099fa081d9582cdfd1fd4cc4dee48486d6f3e5
Patrycja Kolanwska <patrycja.kolanowska@onet.eu>
2019-03-26 15:42:45 +0100

    Poprawione zadanie

--- cw1/zestaw7.R
+++ cw1/zestaw7.R
@@ -1,8 +1,10 @@
 ## Zadanie
 
 ## Przygotować funkcję, która dla danej liczby znajduje sumę jej cyfr
+
 suma_cyfr <- function(liczba){
   digitsum <- sum(floor(liczba / 10^(0:(nchar(liczba) - 1))) %% 10)
+  return(digitsum)
 }
 
 suma = suma_cyfr(liczba = 251)
