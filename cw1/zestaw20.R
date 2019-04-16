f30eec4a4fcea5f7ef728e2b1ce8efc30e1e834b
Daria14 <daritta1996@wp.pl>
2019-04-16 16:17:41 +0200

    rozwiazanie cwiczenia 1

--- cw1/zestaw20.R
+++ cw1/zestaw20.R
@@ -1,12 +1,47 @@
-## Zadanie
-
-## Napisać funkcję znajduja miejsce zerowe pewnej funkcji stosujac algorytm bisekcji
-
-bisekcja = function(f, lewa_krawedz_przedzialu, prawa_krawedz_przedzialu, dokladnosc){
+bisekcja <- function(f, a, b, epsilon)
+{
+  while (abs(a - b) > 2*epsilon){
+    x1 = (a + b) / 2
+    
+    if (f(x1) == 0){
+      break}
+    else if (f(x1) * f(a) < 0){
+      b = x1} 
+    else{
+      a = x1} 
+  }
+  return((a + b) / 2) 
   
 }
+f <- function(x) x^5-x^2-1
 
-bisekcja(sin, 3, 4)
+bisekcja(f,-2,2,0.0001)
 
-sin(3)
-sin(4)
+znak <- function(x){
+  if (x>0){wartosc = 1}
+  else if (x==0){wartosc = 0}
+  else {wartosc = -1}
+  return(wartosc)
+}
+
+mzerowe <- function(f,a,b)
+{
+  skok = 0.01 
+  miejsca_zerowe = c(NULL) 
+  ostatnie_a = a
+  a = a+skok      
+  while (a < b){
+    while (znak(f(a))*znak(f(ostatnie_a))==1){
+      ostatnie_a = a
+      a = a+skok
+      if (a > b) {break}
+    }
+    if (f(a)==0){miejsca_zerowe = c(miejsca_zerowe,a)}
+    else if (a < b){miejsca_zerowe = c(miejsca_zerowe,bisekcja(f,ostatnie_a,a,0.0001)) }
+    ostatnie_a = a
+    a = a+skok 
+  }
+  return(miejsca_zerowe)
+}
+
+mzerowe(sin,-7,7)
